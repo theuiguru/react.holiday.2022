c400edf2055bd4b3d7d2943cf310a434d9641a7d
Chris Thomas <webmaster102@gmail.com>
2022-12-03 23:54:14 -0500

    #3: made a Server Component!

--- app/about/page.jsx
+++ app/about/page.jsx
@@ -1,14 +1,14 @@
 import Image from 'next/image'
 import styles from '../page.module.css'
 
-export default function Home() {
+export default function About() {
   return (
     <div className={styles.container}>
       <main className={styles.main}>
         <h1 className={styles.title}>
           About
         </h1>
-
+        {console.log('where does this log?')}
         <p className={styles.description}>
           Get started by editing{' '}
           <code className={styles.code}>app/page.tsx</code>
