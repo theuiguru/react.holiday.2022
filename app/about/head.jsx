f8d21a45c6db68607083c83857af1c3ac2cdcf40
Chris Thomas <webmaster102@gmail.com>
2022-12-04 23:10:06 -0500

    #4: It's all in your head.js

---
+++ app/about/head.jsx
@@ -0,0 +1,10 @@
+export default function Head() {
+  return (
+    <>
+      <title>About Me</title>
+      <meta content="width=device-width, initial-scale=1" name="viewport" />
+      <meta name="description" content="ct's nextjs 13 app from react.holiday.2022" />
+      <link rel="icon" href="/favicon.ico" />
+    </>
+  )
+}
