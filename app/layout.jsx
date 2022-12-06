24c6bb9579cca316f0ca7384929dee06d3526153
Chris Thomas <webmaster102@gmail.com>
2022-12-05 23:02:14 -0500

    #5: Layouts are finally good!

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,14 +1,10 @@
-import './globals.css'
+import AboutLayout from '/app/about/layout';
+import AboutPage from '/app/about/page';
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      {/*
-        <head /> will contain the components returned by the nearest parent
-        head.jsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
-      */}
-      <head />
-      <body>{children}</body>
-    </html>
+    <AboutLayout>
+      <AboutPage/>
+    </AboutLayout>
   )
 }
