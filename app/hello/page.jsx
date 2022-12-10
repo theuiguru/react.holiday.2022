b31a096c6255cb3969cb738161a93bb0f807f9c5
Chris Thomas <webmaster102@gmail.com>
2022-12-09 22:24:46 -0500

    #9: Fear and loading…

--- app/hello/page.jsx
+++ app/hello/page.jsx
@@ -2,5 +2,7 @@ export default async function HelloPage() {
     let req = await fetch("http://localhost:3000/api/hello");
     let data = await req.json();
 
+    await new Promise((resolve) => setTimeout(resolve, 1000));
+
     return <p>{data.name}</p>;
 }
