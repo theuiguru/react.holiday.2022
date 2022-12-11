ed60d4fe6812c194df0c7933486c0bb50998e6a8
Chris Thomas <webmaster102@gmail.com>
2022-12-10 23:27:13 -0500

    10: Err a-parent

--- app/hello/page.jsx
+++ app/hello/page.jsx
@@ -1,8 +1,11 @@
 export default async function HelloPage() {
-    let req = await fetch("http://localhost:3000/api/hello");
-    let data = await req.json();
+    // let req = await fetch("http://localhost:3000/api/hello");
+    // let data = await req.json();
 
-    await new Promise((resolve) => setTimeout(resolve, 1000));
+    // await new Promise((resolve) => setTimeout(resolve, 1000));
+
+    let res = await fetch('https://httpstat.us/500');
+    let data = await res.json();
 
     return <p>{data.name}</p>;
 }
