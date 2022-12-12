0307d45de082a8cad6ce18de6e37bbecb471893f
Chris Thomas <webmaster102@gmail.com>
2022-12-11 23:59:01 -0500

    11: Hiding in plane site

--- app/hello/error.jsx
+++ app/hello/error.jsx
@@ -1,10 +1,16 @@
 'use client';
 
-export default function HelloError({ error }) {
+import { useEffect } from "react";
+
+export default function Error({ error, reset }) {
+    useEffect(() => {
+        console.log(error);
+    }, [error]);
+
     return (
-        <>
+        <div>
             <p>Something went wrong!</p>
-            <pre>{JSON.stringify(error, null, 2)}</pre>
-        </>
+            <button onClick={() => reset()}>Reset error boundary</button>
+        </div>
     )
 }
