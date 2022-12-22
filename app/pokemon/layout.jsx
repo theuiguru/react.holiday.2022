caf753aa080a88d38ff2313e82efe9efa750309e
Chris Thomas <webmaster102@gmail.com>
2022-12-22 02:18:43 -0500

    21: Build an app video recap 📼

--- app/pokemon/layout.jsx
+++ app/pokemon/layout.jsx
@@ -1,12 +1,6 @@
 export default async function PokemonLayout({ children }) {
   return (
-    <div
-      style={{
-        display: "grid",
-        gridTemplateColumns: "10rem 1fr",
-        padding: "2rem",
-      }}
-    >
+    <div style={{ display: "grid", gridTemplateColumns: "10rem 1fr", padding: "2rem", }}>
       {children}
     </div>
   );
